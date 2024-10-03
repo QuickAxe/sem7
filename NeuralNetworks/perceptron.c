64e64e088fa0a71bbf567fec19db2de49a94772f
quickaxe <manselismyname@gmail.com>
2024-10-03 16:30:38 +0530

    added perceptron

--- NeuralNetworks/perceptron.c
+++ NeuralNetworks/perceptron.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-int activation(int n)
+float activation(int n)
 {
-    return (n >= 0) ? 1 : -1;
+    return (n >= 0) ? 1.0 : -1.0;
 }
 
 int main()
 {
     // "dataset"
-    float x[3][4];
-    float x[0] = {1, -2, 0, -1};
-    float x[1] = {0, 1.5, -0.5, -1};
-    float x[2] = {-1, 1, 0.5, -1};
+    float x[3][4] = {{1, -2, 0, -1},
+                     {0, 1.5, -0.5, -1},
+                     {-1, 1, 0.5, -1}};
 
-    float w[4][4];
-    float w[0] = {1, -1, 0, 0.5};
+    float w[] = {1, -1, 0, 0.5};
 
-    float d[] = {-1, -1, 3};
+    float d[] = {-1, -1, 1};
 
     float c = 0.1;
 
@@ -24,20 +22,24 @@ int main()
 
     for (int i = 0; i < 3; i++)
     {
-        int net = 0;
+        float net = 0;
         for (int j = 0; j < 4; j++)
-            net += w[i][j] * x[i][j];
+            net += w[j] * x[i][j];
 
-        int activationValue = activation(net);
+        float activationValue = activation(net);
 
         // // if the value is the same as the desired value, skip this step
         // if(d[i] == activationValue)
         //     continue;
 
-        int r = d[i] * activationValue;
-        
+        int r = d[i] - activationValue;
 
-
-
+        // update weights
+        for (int j = 0; j < 4; j++)
+            w[j] += (c * r * x[i][j]);
     }
+
+    // output final weights:
+    for (int j = 0; j < 4; j++)
+        printf("\nw[%d] = %f\n", j, w[j]);
 }
